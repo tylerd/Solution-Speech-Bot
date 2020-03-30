69d34ff6b45267b8ae3d4e0eca73e5215a07331d
Tyler Doerksen <tylerd@microsoft.com>
2020-03-30 16:43:26 -0500

    adding Twilio MVC App

---
+++ TwilioApp/TwilioApp.Mvc/Controllers/VoiceController.cs
@@ -0,0 +1,21 @@
+﻿// In Package Manager, run:
+// Install-Package Twilio.AspNet.Mvc -DependencyVersion HighestMinor
+
+using System.Web.Mvc;
+using Twilio.AspNet.Mvc;
+using Twilio.TwiML;
+
+namespace TwilioApp.Mvc.Controllers
+{
+    public class VoiceController : TwilioController
+    {
+        [HttpPost]
+        public ActionResult Index()
+        {
+            var response = new VoiceResponse();
+            response.Say("This is from Tyler. Thank you for calling! Have a great day.");
+
+            return TwiML(response);
+        }
+    }
+}
